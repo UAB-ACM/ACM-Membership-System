49e9f6bae4c85a1e3f614ff3e4af1daeda4718cd
Braden Groom <braden.groom@gmail.com>
2014-09-13 20:06:39 -0500

    removing unnecessary controller aliases

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ acmApp.config(function ($routeProvider) {
         .when('/members', {
             templateUrl: 'partials/members.html',
             controller: 'membersCtrl',
-            controllerAs: 'membersCtrl'
         })
         .when('/members/:blazerId', {
             templateUrl: 'partials/member-profile.html',
@@ -15,32 +14,26 @@ acmApp.config(function ($routeProvider) {
         .when('/add-member', {
             templateUrl: 'partials/add-member.html',
             controller: 'newMemberCtrl',
-            controllerAs: 'newMemberCtrl'
         })
         .when('/renewals', {
             templateUrl: 'partials/renewals.html',
             controller: 'renewalCtrl',
-            controllerAs: 'renewalCtrl'
         })
         .when('/email', {
             templateUrl: 'partials/email.html',
             controller: 'emailCtrl',
-            controllerAs: 'emailCtrl'
         })
         .when('/login', {
             templateUrl: 'partials/login.html',
             controller: 'loginCtrl',
-            controllerAs: 'loginCtrl'
         })
         .when('/settings', {
             templateUrl: 'partials/settings.html',
             controller: 'settingsCtrl',
-            controllerAs: 'settingsCtrl'
         })
         .when('/am-i-a-member', {
             templateUrl: 'partials/am-i-a-member.html',
             controller: 'checkCtrl',
-            controllerAs: 'checkCtrl'
         })
         .otherwise({
             redirectTo: '/login'
