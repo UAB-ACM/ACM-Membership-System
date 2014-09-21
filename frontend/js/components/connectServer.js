356550ebf07a0dfa4279c7d54e1bde0af3c0337a
Braden Groom <braden.groom@gmail.com>
2014-09-21 12:17:42 -0500

    moving main menu derective to index.html so that it is shown on every page.

--- frontend/js/components/connectServer.js
+++ frontend/js/components/connectServer.js
@@ -1,9 +1,17 @@
 $http.get('./config.json').
-    success(function (data, status, headers, config) {
-        localStorage.setItem('baseURL', data.server + ':' + data.port);
-        localStorage.setItem('logo', data.clubLogo);
-        $rootScope.logo = localStorage.getItem('logo');
-    }).
-    error(function (data, status, headers, config) {
-        console.log('There was an error connecting to the server');
+success(function (data, status, headers, config) {
+    localStorage.setItem('baseURL', data.server + ':' + data.port);
+    localStorage.setItem('logo', data.clubLogo);
+    localStorage.setItem('join', data.joinLink);
+    $rootScope.logo = localStorage.getItem('logo');
+    $rootScope.join = localStorage.getItem('join');
+
+    $http.get(localStorage.getItem('baseURL') + '/members').success(function (data, status, headers, config) {
+        members = data.map(function (item) {
+            return item.blazerid
+        });
     });
+}).
+error(function (data, status, headers, config) {
+    console.log('There was an error connecting to the server');
+});
--- frontend/js/directives/mainMenu.js
+++ frontend/js/directives/mainMenu.js
@@ -3,7 +3,5 @@ angular.module('acmApp.directives')
     return {
         restrict: 'E', //E = element, A = attribute, C = class, M = comment         
         templateUrl: 'partials/mainMenu.html',
-        //controller: controllerFunction, //Embed a custom controller in the directive
-        //link: function ($scope, element, attrs) { } //DOM manipulation
     }
 });
